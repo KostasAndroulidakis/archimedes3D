cd53a8c7125198747248749c7dd6d39f88bb815b
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2025-04-03 17:34:33 +0300

    Remove build artifacts and improve .gitignore

--- build/CMakeFiles/BuoyancyDemo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BuoyancyDemo.
-# This may be replaced when dependencies are built.
--- build/rebuild_atmosphere.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Rebuilding AtmosphereDemo..."
-cd /Users/kanon/projects/archimedes3D/build
-make AtmosphereDemo
-echo "Done."
